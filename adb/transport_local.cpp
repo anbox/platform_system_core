994ca48977da11c3f0d1774a75f55f538f4fc703
Simon Fels <morphis@gravedo.de>
2016-06-23 15:29:36 +0200

    adb: work around changed qemu pipe behaviour

--- adb/transport_local.cpp
+++ adb/transport_local.cpp
@@ -248,6 +248,7 @@ static void server_socket_thread(void* arg) {
 #define open    adb_open
 #define read    adb_read
 #define write   adb_write
+#define QEMU_PIPE_FROM_ADB
 #include <qemu_pipe.h>
 #undef open
 #undef read
